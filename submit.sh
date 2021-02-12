e477dad8ea72178d86da740813848a8b981f0a56
CovarianceMomentum <covariancemomentum@gmail.com>
2021-02-12 23:38:29 +0300

    Script fixed

--- submit.sh
+++ submit.sh
@@ -4,3 +4,11 @@ rsync -av --exclude='*Test.java' --exclude='*Tester.java' \
       --exclude='*.iml' --exclude='*module-info.java' \
       ./modules/info.kgeorgiy.ja.фамилия.walk/ \
       ../java-advanced/java-solutions
+
+cd ../java-advanced || exit
+
+git add ./*
+git commit -m "Automated commit"
+git push origin master
+
+cd ../java-advanced-2021 || exit

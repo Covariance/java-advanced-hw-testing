e1a4f922c88e30d95ff0ccbfb9ba308b9a1b5532
CovarianceMomentum <45359233+CovarianceMomentum@users.noreply.github.com>
2021-02-12 19:42:05 +0300

    Automated push added to submit.sh

--- submit.sh
+++ submit.sh
@@ -4,3 +4,7 @@ rsync -av --exclude='*Test.java' --exclude='*Tester.java' \
       --exclude='*.iml' --exclude='*module-info.java' \
       ./modules/info.kgeorgiy.ja.фамилия.walk/ \
       ../java-advanced/java-solutions
+      
+cd ../java-advanced || exit
+
+git push origin master
